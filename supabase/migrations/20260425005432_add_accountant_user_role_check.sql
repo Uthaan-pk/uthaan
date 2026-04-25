aad0f8b0a04ae8502d01849473933e82b89d9708
Pasha17711 <achaudhry@ucsd.edu>
2026-04-25 00:56:02 -0700

    Allow accountant role in user roles

---
+++ supabase/migrations/20260425005432_add_accountant_user_role_check.sql
@@ -0,0 +1,6 @@
+alter table user_roles
+drop constraint if exists user_roles_role_check;
+
+alter table user_roles
+add constraint user_roles_role_check
+check (role in ('superadmin', 'admin', 'teacher', 'student', 'parent', 'accountant'));
